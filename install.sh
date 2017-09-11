7d36f18cbf0f9776a6a1f9ddcc805039e30728b5
JamesNimlos <james.nimlos@csaa.com>
2017-09-11 08:59:03 -0700

    hide desktop files, install brew, node, yarn 🖖

--- install.sh
+++ install.sh
@@ -4,10 +4,25 @@ defaults write com.apple.finder AppleShowAllFiles -bool true
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
+# Hide all desktop icons
+defaults write com.apple.finder CreateDesktop false; killall Finder
+
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=” “
 
 ./upgrade.sh
+
+# install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install node & yarn
+brew install yarn
+
+# install n package manager
+npm install -g n
+
+# take back control of node from brew
+sudo n latest
 
 # see git/.gitconfig for reference
 npm install -g diff-so-fancy
